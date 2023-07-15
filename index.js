c25c2c1663f1aac37243643a12558c8b0f9312fa
Patil Shubhangi Shivraj <122866439+Shubhangip12@users.noreply.github.com>
2023-07-15 12:18:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+   document.getElementById('root')
+);
+
